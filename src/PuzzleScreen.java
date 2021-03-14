32a2238a1b056ec9259a4de9617fbfd6f0c59091
Tim Schneichel <tim@schneichel.de>
2021-03-14 22:54:14 +0100

    Minor additions to code, still no visible TextFields

--- src/PuzzleScreen.java
+++ src/PuzzleScreen.java
@@ -1,31 +1,41 @@
+import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.border.Border;
 
 public class PuzzleScreen {
-
+    private static final Font FONT = new Font("Verdana", Font.CENTER_BASELINE, 20);
     private JPanel rootPanel;
     private JButton generateNewPuzzleButton;
-    private JTextField labelTextField;
+    private JComboBox dimensionDropdown;
     private JTextField labelTextField1;
+    private JPanel SudokuFieldPanel;
+    private JLabel dimensionLabel;
     private JTextField[][] grid;
+    private JPanel[][] minisquarePanels;
 
     public PuzzleScreen() {
-        generateNewPuzzleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                generateGrid();
-            }
-        });
+        generateNewPuzzleButton.addActionListener(actionEvent -> generateGrid());
     }
 
     private void generateGrid() {
-        this.grid = new JTextField[9][9];
-        for (int x = 0; x < 9; x++)
-            for (int y = 0; y < 9; y++){
+        int dimension = ((int) dimensionDropdown.getSelectedItem());
+        this.grid = new JTextField[dimension][dimension];
+        this.SudokuFieldPanel = new JPanel();
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+        Dimension fieldDimension = new Dimension(30, 30);
+        for (int x = 0; x < dimension; x++) {
+            for (int y = 0; y < dimension; y++) {
                 JTextField field = new JTextField();
+                field.setBorder(border);
+                field.setFont(new Font("i",3,4));
+                field.setPreferredSize(fieldDimension);
                 grid[x][y] = field;
+                this.SudokuFieldPanel.add(field);
             }
+        }
+        this.SudokuFieldPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        SudokuFieldPanel.updateUI();
+        SudokuFieldPanel.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -36,4 +46,11 @@ public class PuzzleScreen {
         frame.setVisible(true);
     }
 
+    private void createUIComponents() {
+        dimensionDropdown = new JComboBox();
+        for (int i = 2; i <= 9; i++){
+            dimensionDropdown.addItem(i);
+        }
+        dimensionDropdown.setSelectedItem(8);
+    }
 }
